d303d3756857d74ac54cf60a15f5e25d5989bb52
AlbertZasura <albertalbert841@gmail.com>
2020-11-25 15:15:13 +0700

    update cart images

--- resources/views/cart/update.blade.php
+++ resources/views/cart/update.blade.php
@@ -5,7 +5,7 @@
     <div class="card mb-3">
         <div class="row no-gutters align-items-center">
             <div class="col-md-4">
-                <img src="{{ asset($carts->products->image) }}" class="card-img" alt="{{ $carts->products->name }}">
+                <img src="{{ asset('/storage/images/products/'.$carts->products->image) }}" class="card-img" alt="{{ $carts->products->name }}">
             </div>
             <div class="col-md-8 ">
                 <div class="card-body">
